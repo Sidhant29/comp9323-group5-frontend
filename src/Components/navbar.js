8f5f12acb1f3e0e54ac3905fd4a3070ec66e6115
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-07-05 17:41:39 +1000

    navbar fix

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -8,35 +8,37 @@ import {
    Form,
    FormControl,
    Button,
+   Container,
 } from 'react-bootstrap';
 
 export default function Navigation() {
    return (
       <>
          <Router>
-            <Navbar bg='dark' variant='dark'>
-               <Navbar.Brand>Learning-Projects-Portal</Navbar.Brand>
-               <Nav className='mr-auto'>
-                  <Nav.Item>
-                     <Nav.Link href='/home' style={{ textalign: 'right' }}>
-                        Home
-                     </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                     <Nav.Link href='/login'>Login</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                     <Nav.Link href='/signup'>SignUp</Nav.Link>
-                  </Nav.Item>
-               </Nav>
-               <Form inline>
-                  <FormControl
-                     type='text'
-                     placeholder='Search'
-                     className='mr-sm-2'
-                  />
-                  <Button variant='outline-info'>Search</Button>
-               </Form>
+            <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+               <Container>
+                  <Navbar.Brand href='/home'>
+                     Learning-Projects-Portal
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                  <Navbar.Collapse id='responsive-navbar-nav'>
+                     <Nav className='me-auto'>
+                        <Nav.Link href='/login'>Login</Nav.Link>
+                        <Nav.Link href='/signup'>Signup</Nav.Link>
+                        <Form className='d-flex'>
+                           <FormControl
+                              type='text'
+                              placeholder='Search'
+                              className='mr-sm-2'
+                           />
+                           <Button variant='outline-info'>Search</Button>
+                        </Form>
+                     </Nav>
+                     <Nav>
+                        <Nav.Link href='/my_account'>My account</Nav.Link>
+                     </Nav>
+                  </Navbar.Collapse>
+               </Container>
             </Navbar>
          </Router>
       </>
---
+++ src/Pages/myAccount.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Myaccount() {
+   return (
+      <div>
+         <h1>User Info is displayed here</h1>
+      </div>
+   );
+}
