cc4152abf0dec7729fda89a73cca8666ad635643
Sidhant <sidhant.arora29@gmail.com>
2021-07-09 10:30:52 +1000

    Updated user

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -5,121 +5,166 @@ import {
   Button,
   Form,
   CardDeck,
-  Nav
+  Nav,
+  Badge,
+  Row,
+  Col,
 } from "react-bootstrap";
 import { useState } from "react";
 
 function UserProfile() {
   const [aquiredSkills, setAquiredSkills] = useState(["Swimming", "Java"]);
-  const [showDetails,setShowDetails] = useState(true)
+  const [userName, setUserName] = useState("Name");
+  const [userEmail, setUserEmail] = useState("Email");
+  const [userBio, setUserBio] =
+    useState(`My Bio. This is just a display text, so that we can tell people
+  about ourselves and reach out to other people too.`);
+  const [showDetails, setShowDetails] = useState(true);
   const [desiredSkills, setDesiredSkills] = useState([
     "C++",
     "Api Development",
   ]);
 
-  function MyProjects(){
-      return <div>MY PROJECTS</div>
+  function MyProjects() {
+    return <div>MY PROJECTS</div>;
   }
 
   function UserDetails() {
     return (
       <>
-      <Card style={{marginBottom:"1rem"}}>
-          <Card.Body>
-              Name<br/>
-              Email<br/>
-              Bio<br/>
-              Change password<br/>
-          </Card.Body>
-      </Card>
-      <CardDeck>
-        <Card>
-          <Card.Header>
-            <Card.Title>MY SKILLS </Card.Title>
-            {aquiredSkills.map((element) => {
-              return (
-                <Button variant="success"
-                  style={{ margin: "0.25rem" }}
-                  onClick={(event) => {
-                    console.log(event.target.innerText);
-                    let newArray = [...aquiredSkills].filter(
-                      (item) => item + ` x` !== event.target.innerText
-                    );
-                    setAquiredSkills(newArray);
-                  }}
-                >
-                  {element} x
-                </Button>
-              );
-            })}
-          </Card.Header>
+        <Card style={{ marginBottom: "1rem" }}>
           <Card.Body>
             <Form>
-              <Form.Group controlId="formAquiredSkills">
-                <Form.Control
-                  inline
-                  type="text"
-                  placeholder="Enter skills you wan to mentor for"
-                />
-              </Form.Group>
-              <Button 
-                onClick={() => {
-                  setAquiredSkills([
-                    ...aquiredSkills,
-                    document.getElementById("formAquiredSkills").value,
-                  ]);
-                }}
-                variant="secondary"
-              >
-                Add Skill
-              </Button>
+              <Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Label>My Name</Form.Label>
+                    <Form.Control
+                      placeholder={userName}
+                      onChange={(e) => setUserName(e.target.value)}
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder={userEmail}
+                      onClick={(e) => setUserEmail(e.target.value)}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Group controlId="formBio">
+                    <Form.Label>Your Bio</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      value={userBio}
+                      onChange={(e) => setUserBio(e.target.value)}
+                      placeholder="Share a bit about Yourself"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
             </Form>
           </Card.Body>
         </Card>
-        <Card>
-          <Card.Header>
-            <Card.Title>NOT MY SKILLS </Card.Title>
-            {desiredSkills.map((element) => {
-              return (
-                <Button variant="warning"
-                  style={{ margin: "0.25rem" }}
-                  onClick={(event) => {
-                    console.log(event.target.innerText);
-                    let newArray = [...desiredSkills].filter(
-                      (item) => item + ` x` !== event.target.innerText
-                    );
-                    setDesiredSkills(newArray);
+        <CardDeck>
+          <Card>
+            <Card.Header>
+              <Card.Title>MY SKILLS </Card.Title>
+              {aquiredSkills.map((element) => {
+                return (
+                  <Button variant="success" style={{ margin: "0.25rem" }}>
+                    {element}{" "}
+                    <Badge
+                      onClick={(event) => {
+                        let newArray = [...aquiredSkills].filter(
+                          (item) => item !== element
+                        );
+                        setAquiredSkills(newArray);
+                      }}
+                      variant="dark"
+                    >
+                      x
+                    </Badge>
+                  </Button>
+                );
+              })}
+            </Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group controlId="formAquiredSkills">
+                  <Form.Control
+                    inline
+                    type="text"
+                    placeholder="Enter skills you wan to mentor for"
+                  />
+                </Form.Group>
+                <Button
+                  onClick={() => {
+                    setAquiredSkills([
+                      ...aquiredSkills,
+                      document.getElementById("formAquiredSkills").value,
+                    ]);
                   }}
+                  variant="secondary"
                 >
-                  {element} x
+                  Add Skill
                 </Button>
-              );
-            })}
-          </Card.Header>
-          <Card.Body>
-            <Form>
-              <Form.Group controlId="formDesiredSkills">
-                <Form.Control
-                  inline
-                  type="text"
-                  placeholder="Enter skills you want to learn"
-                />
-              </Form.Group>
-              <Button
-                onClick={() => {
-                  setDesiredSkills([
-                    ...desiredSkills,
-                    document.getElementById("formDesiredSkills").value,
-                  ]);
-                }}
-                variant="secondary"
-              >
-                Add Skill
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </CardDeck>
+              </Form>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Card.Title>NOT MY SKILLS </Card.Title>
+              {desiredSkills.map((element) => {
+                return (
+                  <Button variant="warning" style={{ margin: "0.25rem" }}>
+                    {element}{" "}
+                    <Badge
+                      onClick={(event) => {
+                        console.log(event.target.innerText);
+                        let newArray = [...desiredSkills].filter(
+                          (item) => item !== element
+                        );
+                        setDesiredSkills(newArray);
+                      }}
+                      variant="dark"
+                    >
+                      x
+                    </Badge>
+                  </Button>
+                );
+              })}
+            </Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group controlId="formDesiredSkills">
+                  <Form.Control
+                    inline
+                    type="text"
+                    placeholder="Enter skills you want to learn"
+                  />
+                </Form.Group>
+                <Button
+                  onClick={() => {
+                    setDesiredSkills([
+                      ...desiredSkills,
+                      document.getElementById("formDesiredSkills").value,
+                    ]);
+                  }}
+                  variant="secondary"
+                >
+                  Add Skill
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </CardDeck>
       </>
     );
   }
@@ -138,16 +183,18 @@ function UserProfile() {
         <Card.Header>
           <Nav variant="pills" defaultActiveKey="#first">
             <Nav.Item>
-              <Nav.Link onClick={()=> setShowDetails(true)}>My Details</Nav.Link>
+              <Nav.Link onClick={() => setShowDetails(true)}>
+                My Details
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link onClick={()=> setShowDetails(false)}>My Projects</Nav.Link>
+              <Nav.Link onClick={() => setShowDetails(false)}>
+                My Projects
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Card.Header>
-        <Card.Body>
-        {showDetails?<UserDetails />:<MyProjects/>}
-        </Card.Body>
+        <Card.Body>{showDetails ? <UserDetails /> : <MyProjects />}</Card.Body>
       </Card>
     </div>
   );
